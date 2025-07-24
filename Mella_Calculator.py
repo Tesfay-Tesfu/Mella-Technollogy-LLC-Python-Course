5609d25b7c77a75cffb42e52ca36ba84312e66ac
Tesfay Yemane Tesfu <tesfayphysics@gmail.com>
2025-07-24 16:19:10 -0400

    Add files via upload

---
+++ Mella_Calculator.py
@@ -0,0 +1,12 @@
+#############################
+def add(x,y):# for Addition
+  return x+y
+#############################
+def sub(x,y):# for Subtraction
+  return x-y
+##############################
+def div(x,y):# for Division
+  return x/y
+##############################
+def mult(x,y):# for Multiplication
+  return x*y
